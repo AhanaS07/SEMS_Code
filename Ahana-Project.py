01704cf27e6b11d55a49c72b822779fbb162469e
Ahana Srinath <ahanasrinathmurthy@gmail.com>
2024-10-30 19:50:27 +0530

    Documentation Ahana-Project.py

--- Ahana-Project.py
+++ Ahana-Project.py
@@ -1,17 +1,21 @@
-#Set pin names and board values
-relay = 21
-room = 23
-flame = 5
-buzzer = 24
+#Setting pin names and board values based on RaspberryPi configuration
+relay = 21 #output to light and fan 
+room = 23 #motion sensor
+flame = 5 #flame sensor
+buzzer = 24 #buzzer for alaarm
 green = 19
 red = 16
-#import libraries
+
+#import libraries for inout and output from sensors, time and RFID module
 import RPi.GPIO as GPIO
 import time
 from mfrc522 import SimpleMFRC522
+
+#Clear any anamoly readings as False and declare kind of set up of board
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 scan = SimpleMFRC522()
+
 #Set pins as input and output accordingly
 GPIO.setup(relay, GPIO.OUT)
 GPIO.setup(room, GPIO.IN)
@@ -21,27 +25,33 @@ GPIO.setup(green, GPIO.OUT)
 GPIO.setup(red, GPIO.OUT)
 
 while True:
-	GPIO.output(relay, GPIO.HIGH)
+	GPIO.output(relay, GPIO.HIGH) #switch relay off
+
+	#fire alarm checking
 	fire=GPIO.input(flame)
-	if fire == GPIO.HIGH:
+	if fire == GPIO.HIGH: 
 		GPIO.output(buzzer, GPIO.LOW)
 	else:
 		GPIO.output(buzzer, GPIO.HIGH)
+
+	#Scanning of RFID tag and check to see whether authorised
 	print(“Place your card - “)
 	id, Tag=scan.read()
 	print(id)
-	pre_set=“166061473145”
-	if str(id)!=pre_set:
+	pre_set=“166061473145” #authorised value already configured 
+	if str(id)!=pre_set: #entry denied
 		GPIO.output(green,GPIO.LOW)
 		GPIO.output(red,GPIO.HIGH)
 		time.sleep(2)
 		GPIO.output(red,GPIO.LOW)
 		continue
-	elif str(id)==pre_set:
+	elif str(id)==pre_set: #entry allowed 
 		GPIO.output(green,GPIO.HIGH)
 		GPIO.output(red,GPIO.LOW)
 		time.sleep()
 		GPIO.output(green,GPIO.LOW)
+
+		#loop to detect for motion once room is entered by authorised person
 		while True:
 			motion = GPIO.input(room)
 			fire = GPIO.input(flame)
